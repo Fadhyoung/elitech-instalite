c2f55ae0cbea4a86cf59d2cc604f8854b86039ca
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-04-12 20:34:29 +0700

    tweak: update feed modal

--- resources/views/profile/partials/feed-modal.blade.php
+++ resources/views/profile/partials/feed-modal.blade.php
@@ -45,9 +45,12 @@
                     </div>
                 </div>
 
-                <!-- Action buttons and likes -->
-                <div class="border-t p-4">
-                    <div class="flex justify-between mb-2">
+
+
+                <!-- COMMENT SECTION -->
+                <div class="flex flex-col">
+                    <!-- Action buttons and likes -->
+                    <div class="flex justify-between mb-2 border-t p-4">
                         <div class="flex gap-4">
                             <!-- Archive Button (only show if not archived) -->
                             <button x-show="!selectedFeed.archived" @click="archiveFeed(selectedFeed.id)">
@@ -61,7 +64,25 @@
                         </div>
                     </div>
                 </div>
-
+                <div x-data="{ comment: '' }" class="px-5 flex items-center justify-between py-2 border-t p-4">
+                    <button class="mr-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+                        </svg>
+                    </button>
+                    <div class="flex items-center flex-1">
+                        <input
+                            type="text"
+                            placeholder="Add a comment..."
+                            class="flex-1 border-none outline-none text-sm focus:ring-0"
+                            x-model="comment" />
+                    </div>
+                    <button
+                        :class="comment.length > 0 ? 'text-sm font-semibold text-blue-500' : 'text-sm font-semibold text-blue-300'"
+                        :disabled="comment.length === 0">
+                        Post
+                    </button>
+                </div>
             </div>
             <button @click="closeModal" class="absolute p-10 top-0 right-0 text-white">Close</button>
         </div>
